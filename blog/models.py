7e0033ed6cda5c31c7561150d97ea3c99c7a3b49
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-01-18 12:35:50 +0100

    Changed models and made more migrations, also add post and comments to db

--- blog/models.py
+++ blog/models.py
@@ -2,21 +2,22 @@ from django.db import models
 
 # Create your models here.
     
-class Comments(models.Model):
+class Post(models.Model):
+    name = models.CharField(max_length=250)
     text = models.TextField()
-    autor = models.CharField(max_length=100)
     date = models.DateField(auto_now_add=True)
     date_with_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
     
-class Post(models.Model):
-    name = models.CharField(max_length=250)
+    
+class Comments(models.Model):
     text = models.TextField()
+    autor = models.CharField(max_length=100)
     date = models.DateField(auto_now_add=True)
     date_with_time = models.DateTimeField(auto_now_add=True)
-    comments = models.ForeignKey(Comments, on_delete=models.PROTECT)
+    many_to_many = models.ManyToManyField("Post", related_name="Many_to_many")
 
     def __str__(self):
         return self.name
--- main.py
+++ main.py
@@ -2,3 +2,33 @@ import django_setup
 
 from blog.models import Post, Comments
 
+
+#!--Creating post and some comments related to this post
+#first_post = Post.objects.create(
+#    name = "Як встановити пайтон?",
+#    text = "Я хочу навчитися програмувати на пайтоні, як його правильно встановити, щоб потім не вертатися до цього.",
+#
+#)
+#
+#first_comment = Comments.objects.create(
+#    text = "Глянь відео на ютубі, я хз",
+#    autor = "Сергій2010",
+#    #related_to_post = first_post,
+#
+#)
+#
+#second_comment = Comments.objects.create(
+#    text = "Руками",
+#    autor = "Степан[Prime]",
+#    #related_to_post = first_post,
+#    
+#)
+
+#!--Finding post and comments by their id
+first_post = Post.objects.get(id = 1)
+first_comment = Comments.objects.get(id = 1)
+second_comment = Comments.objects.get(id = 2)
+
+#!--Relating comments to post
+#first_comment.many_to_many.add(first_post)
+#second_comment.many_to_many.add(first_post)
